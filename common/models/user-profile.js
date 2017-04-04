2ca0873e8300c64a9c110a66fe07d7f6532f76e3
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-04-04 13:36:35 -0300

    Creating the http request for the user login.

--- common/models/user-profile.js
+++ common/models/user-profile.js
@@ -6,13 +6,16 @@ module.exports = function(UserProfile) {
 
   //Method used to register a user on the application
   UserProfile.signUp = function(user, callback) {
+    //Regex used to validate the inputs of the user registration
     const emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
 
+    //Used to cryptograph the password of the user, to ensure security
     if (passwordRegex.test(user.password)) {
       user.password = CryptoJS.AES.encrypt(user.password, secret);
     }
 
+    //Used to store the user registration on the database
     if (emailRegex.test(user.email)) {
       UserProfile.upsert(user, function(err, obj) {
         if (err) {
@@ -26,11 +29,39 @@ module.exports = function(UserProfile) {
     callback(null, user);
   };
 
-  //Used for submition of sign up form for the user
+  //Used for the submition of sign up form for the user
   UserProfile.remoteMethod('signUp', {
     http: {path: '/sign-up', verb: 'post'},
     accepts: {arg: 'user', type: 'UserProfile',
               required: true, http: {source: 'body'}},
     returns: {arg: 'status', type: 'string'},
   });
+
+  //Used to authenticate the user's access to the system
+  UserProfile.login = function (user, callback) {
+
+    UserProfile.findOne({where: {'email': user.email}}, function (err, obj){
+      if (obj != null) {
+        var password = CryptoJS.decrypt(obj.password.toString(), secret);
+        if (user.password == password) {
+          callback(null, '200');
+        }
+        else {
+          callback(null, '400');
+        }
+      }
+      else {
+        callback(null, '400');
+      }
+    })
+  }
+
+  //Used for the submition of the access of the user on the system
+  UserProfile.remoteMethod('logIn', {
+    http: {path: '/login', verb: 'post'},
+    accepts: {arg: 'user', type: 'UserProfile',
+              required: true, http: {source: 'body'}},
+    returns: {arg: 'status', type: 'string'}
+  });
+
 };
