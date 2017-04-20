ec42c62e582858214f61263400d197995cc2064a
lucaaas <lucasoliveira2306@gmail.com>
2017-04-20 14:55:57 -0300

    Added after clause and token verification in test login

--- test/login.test.js
+++ test/login.test.js
@@ -4,14 +4,23 @@ var chaiHttp  = require('chai-http');
 var server = 'http://localhost:3000';
 var UserProfile = require('../common/models/user-profile');
 
+const mongoose = require('mongoose');
+
 chai.use(chaiHttp);
 
-var user = {firstName: 'Jesus',
-            lastName: 'Cristo',
-            email: 'jesus123314@hotmail.com',
+var user = {firstName: 'Lucas',
+            lastName: 'Oliveira',
+            email: 'hugo@hotmail.com',
             password: 'Teste123'};
 
 before(function(done) {
+  mongoose.connect('mongodb://localhost/organizae');
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error'));
+  db.once('open', function() {
+    console.log('We are connected to test database!');
+  });
+
   //Creates user in system's database by signing them up
   chai.request(server)
       .post('/api/UserProfiles/sign-up')
@@ -23,6 +32,12 @@ before(function(done) {
   done();
 });
 
+after(function(done) {
+  mongoose.connection.db.dropDatabase(function() {
+    mongoose.connection.close(done);
+  });
+});
+
 describe('Test sign-in', function() {
   it('should sign-in with sucess', function(done) {
     var loginParams = {email: user['email'],
@@ -32,7 +47,7 @@ describe('Test sign-in', function() {
         .send(loginParams)
         .end((err, res) => {
           expect(err).to.not.exist;
-          expect(res.body).to.not.be.empty;
+          expect(res.body.token).to.not.be.a('null');
           done();
         });
   });
