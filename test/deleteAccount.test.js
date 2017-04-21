065b51ab22cfe628a9e23c0aae123b58f1c51f0d
Vitor Menezes <torandoing@gmail.com>
2017-04-21 16:18:25 -0300

    Added test for error when deleting unregistered user

--- test/deleteAccount.test.js
+++ test/deleteAccount.test.js
@@ -9,16 +9,20 @@ const mongoose = require('mongoose');
 
 chai.use(chaiHttp);
 
+// Creates user to be registered
 var user = {firstName: 'Jesus',
             lastName: 'Cristo',
             email: 'jesuscristo@gmail.com',
             password: 'Teste123'};
 
+// Creates Schema to be queried on database
 var userProfileSchema = new Schema({email: 'string'});
 
+// Creates model based on schema to be queried on database
 var userProfile = mongoose.model('UserProfile', userProfileSchema);
 
 before(function(done) {
+  // Connects to database
   mongoose.connect('mongodb://localhost/organizae');
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error'));
@@ -38,6 +42,7 @@ before(function(done) {
 });
 
 after(function(done) {
+  // Deletes the test's database
   mongoose.connection.db.dropDatabase(function() {
     mongoose.connection.close(done);
   });
@@ -45,19 +50,49 @@ after(function(done) {
 
 describe('Test delete', function() {
   it('should delete account with sucess', function(done) {
-    //var query = UserProfile.where({email: user.email});
+    //Sends request to delete user's account
     chai.request(server)
         .post('/api/UserProfiles/delete')
         .send(user)
         .end((err, res) => {
           expect(err).to.not.exist;
-          expect(res).to.have.status(200);
+          console.log(res.body.status);
+          expect(res.body.status).to.equal(200);
+
           expect(res.body.token).to.not.exist;
 
+          // Checks whether or not the user was deleted
           userProfile.findOne({where: {'email': user.email}}, function(err, userProfile) {
             expect(userProfile).to.not.exist;
             expect(err).to.exist;
           });
+
+          done();
+        });
+  });
+
+  it('should try to delete account that does not exist and get an error', function(done) {
+    // Creates a fake user who's not registered in the system
+    var fakeUser = {firstName: 'Fake',
+                lastName: 'User',
+                email: 'fakeuser@gmail.com',
+                password: 'Teste123'};
+
+    // Sends request to try to delete unregistered user
+    chai.request(server)
+        .post('/api/UserProfiles/delete')
+        .send(fakeUser)
+        .end((err, res) => {
+          expect(err).to.not.exist;
+          expect(res.body.status).to.equal(400);
+          expect(res.body.token).to.not.exist;
+
+          // Confirms that unregistered user is not in the database
+          userProfile.findOne({where: {'email': user.email}}, function(err, userProfile) {
+            expect(userProfile).to.not.exist;
+            expect(err).to.exist;
+          });
+
           done();
         });
   });
