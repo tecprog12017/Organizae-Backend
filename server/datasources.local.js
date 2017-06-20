ee55e9f99a6d8af6217f4ca4db420a2be2fe5a70
Hugo Neves de Carvalho <hugo-nc@hotmail.com>
2017-06-20 14:46:30 -0300

    Changed datasources file name

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,7 +1,7 @@
 module.exports = {
-  organizae: {
+  db: {
     connector: 'mongodb',
-    hostname: process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost',
+    hostname: process.env.OPENSHIFT_MONGODB_DB_HOST || '127.0.0.1',
     port: process.env.OPENSHIFT_MONGODB_DB_PORT || 27017,
     user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
     password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
