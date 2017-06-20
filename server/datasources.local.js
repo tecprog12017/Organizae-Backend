ee36814be91b416e2ded1d8b934c009731a475ec
Hugo Neves de Carvalho <hugo-nc@hotmail.com>
2017-06-20 14:51:30 -0300

    changed host

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,7 +1,7 @@
 module.exports = {
   db: {
     connector: 'mongodb',
-    hostname: process.env.OPENSHIFT_MONGODB_DB_HOST || '127.0.0.1',
+    hostname: process.env.OPENSHIFT_MONGODB_DB_HOST,
     port: process.env.OPENSHIFT_MONGODB_DB_PORT || 27017,
     user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
     password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
